f242a4d31fd3e13cd6c24ef4e9a556d89d67b4a3
Benjamin Reed <ranger@opennms.org>
2016-05-27 16:56:56 -0400

    skip minion image if RPMs do not exist

--- docker/build-docker-images.sh
+++ docker/build-docker-images.sh
@@ -7,8 +7,12 @@ docker pull postgres:9.5.1
 echo "Building base image"
 docker build -t stests/base ./base
 
-echo "Building Minion image"
-docker build -t stests/minion ./minion
+if [ `ls -1 minion/rpms/*.rpm | wc -l` -gt 0 ]; then
+	echo "Building Minion image"
+	docker build -t stests/minion ./minion
+else
+	echo "WARNING: No minion RPMs.  Skipping minion image."
+fi
 
 echo "Building OpenNMS image"
 docker build -t stests/opennms ./opennms
