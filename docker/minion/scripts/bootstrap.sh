fed4be94056240d40417c22ecc5355914ce83c63
Dustin Frisch <fooker@lab.sh>
2016-09-28 14:07:36 +0200

    Use failover ActiveMQ connection for automatic reconnectiong during container startup

--- docker/minion/scripts/bootstrap.sh
+++ docker/minion/scripts/bootstrap.sh
@@ -13,7 +13,7 @@ sed -i s/rmiServerHost.*/rmiServerHost=0.0.0.0/g "${MINION_HOME}/etc/org.apache.
 echo "location = MINION" > $MINION_HOME/etc/org.opennms.minion.controller.cfg
 echo "id = 00000000-0000-0000-0000-000000ddba11" >> $MINION_HOME/etc/org.opennms.minion.controller.cfg
 echo "http-url = http://${OPENNMS_PORT_8980_TCP_ADDR}:${OPENNMS_PORT_8980_TCP_PORT}" >> $MINION_HOME/etc/org.opennms.minion.controller.cfg
-echo "broker-url = tcp://${OPENNMS_PORT_61616_TCP_ADDR}:${OPENNMS_PORT_61616_TCP_PORT}" >> $MINION_HOME/etc/org.opennms.minion.controller.cfg
+echo "broker-url = failover:tcp://${OPENNMS_PORT_61616_TCP_ADDR}:${OPENNMS_PORT_61616_TCP_PORT}" >> $MINION_HOME/etc/org.opennms.minion.controller.cfg
 
 echo "Overlaying files from /minion-docker-overlay/ onto ${MINION_HOME}"
 find /minion-docker-overlay -ls
