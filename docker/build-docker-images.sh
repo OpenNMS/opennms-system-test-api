5d541f998a8ef1ad19443a3a3594c75c2aeefe26
Benjamin Reed <ranger@opennms.org>
2016-10-19 14:09:33 -0400

    improved logging

--- docker/build-docker-images.sh
+++ docker/build-docker-images.sh
@@ -1,27 +1,32 @@
 #!/bin/sh -e
 [ ! -d minion ] && echo "The script must be invoked from the docker subdirectory of the project." && exit 1
 
+run() {
+	echo "Running: $@"
+	"$@"
+}
+
 echo "Pulling postgres image from public registry"
-docker pull postgres:9.5.1
+run docker pull postgres:9.5.1
 
 echo "Building base image"
-docker build -t stests/base ./base
+run docker build -t stests/base ./base
 
 echo "Building Ubuntu base image"
-docker build -t stests/ubuntu-base ./ubuntu-base
+run docker build -t stests/ubuntu-base ./ubuntu-base
 
 if [ `ls -1 minion/rpms/*.rpm | wc -l` -gt 0 ]; then
 	echo "Building Minion image"
-	docker build -t stests/minion ./minion
+	run docker build -t stests/minion ./minion
 else
 	echo "WARNING: No minion RPMs.  Skipping minion image."
 fi
 
 echo "Building OpenNMS image"
-docker build -t stests/opennms ./opennms
+run docker build -t stests/opennms ./opennms
 
 echo "Building snmpd image"
-docker build -t stests/snmpd ./snmpd
+run docker build -t stests/snmpd ./snmpd
 
 echo "Building Tomcat image"
-docker build -t stests/tomcat ./tomcat
+run docker build -t stests/tomcat ./tomcat
